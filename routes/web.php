b638ac59da53ac47b2f1fe45b0ad5e054c1dcafb
= <=>
2024-04-12 21:24:22 -0500

    5. Dashboard Files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +20,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
